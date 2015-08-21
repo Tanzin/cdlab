ac056bf901482c98952d736f2f9516cd86fb02a2
tanzin <tanzin@gmail.com>
2015-08-21 16:14:15 +0530

    first commit from lab

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cdlab
+
 compiler design lab assignments
+1: Lexical analizer
+2: Error handling for exponential numbers
---
+++ heloworld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<"helo world";
+	return 0;
+}
